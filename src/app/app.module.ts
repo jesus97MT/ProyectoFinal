0309d290601171ad9e991a8db04d4dc5d80202f1
Jesús Martínez Torrecilla <jesusmartorrecilla@gmail.com>
2018-05-02 20:21:03 +0200

    Added SideNav (Header)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
