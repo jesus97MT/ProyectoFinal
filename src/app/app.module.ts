cedaaed1e9974afd8a043a1e4efb6a1ce6d50813
Jesús Martínez Torrecilla <jesusmartorrecilla@gmail.com>
2018-05-05 10:29:25 +0200

    Minnor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { HeaderComponent } from './header/header.component';
+import { MainFormComponent } from './main-form/main-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-form/main-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-form',
+  templateUrl: './main-form.component.html',
+  styleUrls: ['./main-form.component.css']
+})
+export class MainFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
